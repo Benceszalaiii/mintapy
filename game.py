078aeb939644c079fc9cb7c4c29d4d5c8c4d4b94
Benceszalaiii <szalai.bence.bendeguz@students.jedlik.eu>
2024-02-02 08:14:22 +0100

    alap fileok init

---
+++ game.py
@@ -0,0 +1,10 @@
+import pygame
+
+class Game:
+    @property
+    def run(self):
+        return self.run()
+
+    def __init__(self):
+        self.score = 0
+        pygame.init()
---
+++ main.py
@@ -0,0 +1,6 @@
+from game import Game
+def main():
+    game = Game()
+    game.start()
+if __name__ == "__main__":
+    main()
